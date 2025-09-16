6456786a8afe7eec0dbe3ecfe9ba6dcaf3d62120
Christoper Chenoa <christoperchenoa4664@gmail.com>
2025-09-16 10:50:09 +0700

    B Task Congratulate user

--- LW01_BINUS_Semester_1.c
+++ LW01_BINUS_Semester_1.c
@@ -1,9 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
+// A Task. Print Hello World!
+//  printf("Hello World!\n");
 
- printf("Hello World!\n");
-
+// B Task. Congratulate user
+  char name[120];
+  scanf("%[^\n]", name);
+  printf("Congrats \"%s\" for joining the CS department at BINUS.\n", name);
 
   return 0;
 }
