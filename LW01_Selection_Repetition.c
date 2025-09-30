c51bdc4453af866012cf92bcbf39c48afcc68922
Christoper Chenoa <christoperchenoa4664@gmail.com>
2025-09-30 18:39:04 +0700

    ATask

--- LW01_Selection_Repetition.c
+++ LW01_Selection_Repetition.c
@@ -4,20 +4,24 @@ int main(){
   //Three Elevator
   int x, y, k, i;
   scanf ("%d %d %d", &x, &y, &k); getchar();
-  for (i= 1;i<100; ) {
-    x++;
-    y--;
-    if (x==y && y==k) {
-      printf ("1\n");
-      break;
-    } else if (x>y) {
-        printf ("-1\n");
+  for (i = y ; i == 1; i--) {
+
+    if (x >= 0 && y >= 0 && k >= 0 && x <= 100 && y <= 100 && k <= 100) {
+      if (x==y && y==k ) {
+        printf ("1\n");
         break;
+      } else if (x>y) {
+      printf ("-1\n");
+      break; }
+      else{
+        x++;
+        y--;
+      }
+    }
+    else {
+      printf ("-1\n");
+      break;
     }
   }
-
-
-
-
   return 0;
 }
